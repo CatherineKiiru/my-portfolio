d49587b1cf471de3a0326e15d8ef3f676f789362
Catherine <wambukukiiru@gmail.com>
2022-03-09 14:47:20 +0300

    created product components

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,7 @@
 import "./about.css"
 import random from "../../img/random.jpg"
 
+
 const About = () => {
     return (
         <div className="a">
@@ -13,16 +14,21 @@ const About = () => {
                     className="a-img"/>
                 </div>
             </div>
+            
             <div className="a-right">
                 <h1 className="a-title">About Me</h1>
                 <p className="a-sub">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 </p>
                 <p className="a-desc">
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                   </p>
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                </p>
+                   {/*
+                   <div className="a-award">
+                       <img src={Award} alt="" className="a-award-img"/>
+                   </div> */}
             </div>
         </div>
     )
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./product.css"
+
+const product = () => {
+    return (
+        <div>
+            card
+        </div>
+    )
+}
+
+export default product
