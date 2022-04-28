ab5f31ac7a95a31d93ebc0f6024bdb1268f1f4f1
Catherine <wambukukiiru@gmail.com>
2022-04-28 21:38:53 +0300

    completed product list component and added contact component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import "./contact.css"
+import Phone from "../../img/phone.png"
+import Email from "../../img/email.png"
+import Address from "../../img/address.png"
+
+function Contact() {
+    return (
+        <div className="c">
+            <div className="c-bg"></div>
+            <div className="c-wrapper">
+                <div className="c-left">
+                    <h1 className="c-title">Let's discuss your project</h1>
+                    <div className="c-info">
+                        <div className="c-info-item">
+                            <img src={Phone} alt="" 
+                            className="c-icon"/>
+                            +254 700 660 770
+                        </div>
+                        <div className="c-info-item">
+                            <img src={Email} alt="" 
+                            className="c-icon"/>
+                            info@info.com
+                        </div>
+                        <div className="c-info-item">
+                            <img src={Address} alt="" 
+                            className="c-icon"/>
+                            Nairobi, Kenya
+                        </div>
+                    </div>
+                </div>
+                <div className="c-right">
+                    <p className="c-desc">
+                        <b>What's your story</b>Get in touch. I 
+                        will help you turn your website into a leads powerhouse
+                    </p>
+                    <form>
+                       <input type="text" placeholder="Name" name="user_name"/>
+                       <input type="text" placeholder="Subject" name="user_subject"/>
+                       <input type="text" placeholder="Email" name="user_email"/>
+                       <textarea rows="5" placeholder="Message"></textarea>
+                       <button>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
--- src/data.js
+++ src/data.js
@@ -1,33 +1,33 @@
 export const products = [
     {
         id: 1,
-        img: "https://img.freepik.com/free-vector/muslim-ramadan-kareem-festival-greeting-design_1017-30817.jpg?t=st=1650553949~exp=1650554549~hmac=d451865fe7b728851f386f100f3f79c2c3375e3188a3d0819699b551e01c1cc5&w=740",
+        img: "https://img.freepik.com/free-vector/store-with-credit-card-gift-boxes-buyers-illustration_1262-18980.jpg?t=st=1651142665~exp=1651143265~hmac=7e692189e69b5ecf34e37543c3744aae2cd6530ce83d5219f265a77fa804a0d8&w=996",
         link: "https://github.com/CatherineKiiru/ecommerce-site",
     },
     {
         id: 2,
-        img: "src/img/address.png",
+        img: "https://img.freepik.com/free-vector/property-insurance-isometric-landing-page-banner_107791-756.jpg?t=st=1651142732~exp=1651143332~hmac=36ff9be2d9918e84a955a2ba26e65eaac5b57af3a1571ff9067aada5561a245c&w=996",
         link: "https://github.com/CatherineKiiru/ecommerce-site",
 
     },
     {
         id: 3,
-        img: "src/img/address.png",
+        img: "https://img.freepik.com/free-vector/fintech-isometric-landing-page-with-bank-building-money-financial-technologies-digital-solutions-banking-business-software-mobile-app-finance-services-3d-line-art-web-banner_107791-3602.jpg?w=1060&t=st=1651142787~exp=1651143387~hmac=2e27ab614be941990eb5d097406d0d61f4776f9b8efcdc996a6e178c24f3a59e",
         link: "https://github.com/CatherineKiiru/ecommerce-site",
     },
     {
         id: 4,
-        img: "src/img/address.png",
+        img: "https://img.freepik.com/free-vector/online-library-app-reading-banner_33099-1733.jpg?t=st=1651142800~exp=1651143400~hmac=0e15bbdac6d4447721d9e5b9aef7819e819660f2b09b064b2ace9d5f7a59eb86&w=1060",
         link: "https://github.com/CatherineKiiru/ecommerce-site",
     },
     {
         id: 5,
-        img: "src/img/address.png",
+        img: "https://img.freepik.com/free-vector/diagnostics-center-landing-page_107791-5466.jpg?w=996&t=st=1651143082~exp=1651143682~hmac=324276ee7acb0e2f2e5093be5eb4924e0f15181e396d100a303ea3ca75df8365",
         link: "https://github.com/CatherineKiiru/ecommerce-site",
     },
     {
         id: 6,
-        img: "src/img/address.png",
+        img: "https://img.freepik.com/free-vector/fintech-isometric-landing-page-with-bank-building-money-financial-technologies-digital-solutions-banking-business-software-mobile-app-finance-services-3d-line-art-web-banner_107791-3602.jpg?w=1060&t=st=1651142787~exp=1651143387~hmac=2e27ab614be941990eb5d097406d0d61f4776f9b8efcdc996a6e178c24f3a59e",
         link: "https://github.com/CatherineKiiru/ecommerce-site",
     },
 
